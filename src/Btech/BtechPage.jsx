00477d851a97030fcdccbae8b644e81448db09df
Harsh231075 <harshsingh231075@gmail.com>
2025-01-14 22:04:27 +0530

    update

--- src/Btech/BtechPage.jsx
+++ src/Btech/BtechPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Page1 from './page1'
-import Page2 from './page2'
+import Page1 from './Page1'
+import Page2 from './Page2'
 import AdmissionProcess from './AdmissionProcess'
 
 function BtechPage() {
